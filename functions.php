bd433bad750d7ac99c170366baf855c17b07965a
Erin Bell <orchard@Erins-MacBook-Pro.local>
2017-12-05 14:45:04 -0500

    conditional bg img loading

--- functions.php
+++ functions.php
@@ -482,6 +482,95 @@ function clean_header(){
   wp_deregister_script( 'comment-reply' );
 }
 add_action('init','clean_header');
+function erin_custom_background_cb() {
+    // $background is the saved custom image, or the default image.
+    $background = set_url_scheme( get_background_image() );
+ 
+    // $color is the saved custom color.
+    // A default has to be specified in style.css. It will not be printed here.
+    $color = get_background_color();
+ 
+    if ( $color === get_theme_support( 'custom-background', 'default-color' ) ) {
+        $color = false;
+    }
+ 
+    if ( ! $background && ! $color ) {
+        if ( is_customize_preview() ) {
+            echo '<style type="text/css" id="custom-background-css"></style>';
+        }
+        return;
+    }
+ 
+    $style = $color ? "background-color: #$color;" : '';
+ 
+    if ( $background ) {
+        $image = ' background-image: url("' . esc_url_raw( $background ) . '");';
+ 
+        // Background Position.
+        $position_x = get_theme_mod( 'background_position_x', get_theme_support( 'custom-background', 'default-position-x' ) );
+        $position_y = get_theme_mod( 'background_position_y', get_theme_support( 'custom-background', 'default-position-y' ) );
+ 
+        if ( ! in_array( $position_x, array( 'left', 'center', 'right' ), true ) ) {
+            $position_x = 'left';
+        }
+ 
+        if ( ! in_array( $position_y, array( 'top', 'center', 'bottom' ), true ) ) {
+            $position_y = 'top';
+        }
+ 
+        $position = " background-position: $position_x $position_y;";
+ 
+        // Background Size.
+        $size = get_theme_mod( 'background_size', get_theme_support( 'custom-background', 'default-size' ) );
+ 
+        if ( ! in_array( $size, array( 'auto', 'contain', 'cover' ), true ) ) {
+            $size = 'auto';
+        }
+ 
+        $size = " background-size: $size;";
+ 
+        // Background Repeat.
+        $repeat = get_theme_mod( 'background_repeat', get_theme_support( 'custom-background', 'default-repeat' ) );
+ 
+        if ( ! in_array( $repeat, array( 'repeat-x', 'repeat-y', 'repeat', 'no-repeat' ), true ) ) {
+            $repeat = 'repeat';
+        }
+ 
+        $repeat = " background-repeat: $repeat;";
+ 
+        // Background Scroll.
+        $attachment = get_theme_mod( 'background_attachment', get_theme_support( 'custom-background', 'default-attachment' ) );
+ 
+        if ( 'fixed' !== $attachment ) {
+            $attachment = 'scroll';
+        }
+ 
+        $attachment = " background-attachment: $attachment;";
+ 
+        $style .= $image . $position . $size . $repeat . $attachment;
+    }
+?>
+<script>
+	// conditional load of bg img
+	function updateViewportDimensions() {
+		var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth
+		||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight;
+		return { width:x,height:y };
+	}
+	var viewport = updateViewportDimensions();
+	if(viewport.width > 590){
+		var css = 'body.custom-background { <?php echo trim( $style ); ?> }',
+		    head = document.head || document.getElementsByTagName('head')[0],
+		    style = document.createElement('style');
+		style.type = 'text/css';
+		if (style.styleSheet){
+		  style.styleSheet.cssText = css;
+		} else {
+		  style.appendChild(document.createTextNode(css));
+		}
+		head.appendChild(style);
+	}
+</script>
 
 /************* Remove jQuery *************/
 function wpdocs_dequeue_script() {
